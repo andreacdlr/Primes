b684c606d5667402651c059c0337707fdfc19b9e
andreacdlr <andrea.crucesdelaroca@gmail.com>
2015-10-13 22:36:01 -0500

    inicial commit

---
+++ javascript/app.js
@@ -0,0 +1,44 @@
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 500) {
+        
+    	$("header nav").css("background-color", "#0DC0C0");
+    } else {
+       $("header nav").css("background-color", "transparent");
+    }
+});
+
+
+
+
+
+
+
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 8000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+  
+
+});
+
+
